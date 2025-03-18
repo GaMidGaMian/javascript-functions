b389ad16aff151e9f9a86061ab0a94af34f847b3
Pattarasak Raksanarong <rogerlovek@gmail.com>
2025-03-18 19:31:24 +0700

    finished assignment

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,25 @@
-// Start coding here
-let calculator = {};
+let calculator = {add, subtract, multiply, divide}
+   
+
+function add (a, b) {
+        return a + b;
+    }
+
+    function subtract (a, b) {
+        return a - b;
+    }
+
+    function multiply(a, b) {
+        return a * b;
+    }
+
+    function divide(a, b) {
+        return a / b;
+    }
+
+
+let addResult = add(10, 20);
+console.log(addResult)
+
+let divideResult = divide(3000, 10)
+console.log(divideResult)
